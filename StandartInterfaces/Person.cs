df537f102e4670a99b3e6c2cb39f1586047d154e
Constantine Philipchenko <const-phi@ya.ru>
2018-03-20 22:54:21 +0300

    Added an example of using IComparable<in T> interface. Class Person was used to bring to concrete class.

---
+++ StandartInterfaces/Person.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StandartInterfaces
+{
+    public sealed class Person
+    {
+        private String lastName;
+
+        public String LastName
+        {
+            get { return lastName; }
+            private set
+            {
+                var trimedValue = value.Trim();
+                if (String.IsNullOrEmpty(trimedValue))
+                    throw new ArgumentException("Wrong argument, last name must be not empty, null or full whitespaces.", nameof(LastName));
+                lastName = trimedValue;
+            }
+        }
+
+        private String firstName;
+
+        public String FirstName
+        {
+            get { return firstName; }
+            private set
+            {
+                var trimedValue = value.Trim();
+                if (String.IsNullOrEmpty(trimedValue))
+                    throw new ArgumentException("Wrong argument, first name must be not empty, null or full whitespaces.", nameof(FirstName));
+                firstName = trimedValue;
+            }
+        }
+
+        private String middleName;
+
+        public String MiddleName
+        {
+            get { return middleName; }
+            private set
+            {
+                var trimedValue = value.Trim();
+                if (String.IsNullOrEmpty(trimedValue))
+                    throw new ArgumentException("Wrong argument, middle name must be not empty, null or full whitespaces.", nameof(MiddleName));
+                middleName = trimedValue;
+            }
+        }
+
+        private DateTime birthDate;
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            private set
+            {
+                if (value > DateTime.Now)
+                    throw new ArgumentOutOfRangeException(nameof(BirthDate));
+                birthDate = value;
+            }
+        }
+
+        private DateTime? deathDate;
+
+        public DateTime? DeathDate
+        {
+            get { return deathDate; }
+            private set
+            {
+                if (value != null && (value < BirthDate || value > DateTime.Now))
+                    throw new ArgumentOutOfRangeException(nameof(DeathDate));
+                deathDate = value;
+            }
+        }
+
+        public Int32 Age => (DeathDate?.Year ?? DateTime.Now.Year) - BirthDate.Year;
+
+        public Person(String fullName, DateTime birthDate, DateTime? deathDate = null)
+        {
+            var names = fullName.Split(' ');
+            if (names.Length < 3)
+                throw new ArgumentException("Illegal format full name representation.", nameof(fullName));
+
+            LastName = names[0];
+            FirstName = names[1];
+            MiddleName = names[2];
+
+            BirthDate = birthDate;
+            DeathDate = deathDate;
+        }
+
+        public Person(String lastName, String firstName, String middleName, DateTime birthDate,
+            DateTime? deathDate = null)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            MiddleName = middleName;
+
+            BirthDate = birthDate;
+            DeathDate = deathDate;
+        }
+
+        public override String ToString() => 
+            $"{LastName} {FirstName} {MiddleName} ({BirthDate.Year} - {DeathDate?.Year})";
+
+        public sealed class LastNameComparer : IComparer<Person>
+        {
+            public Int32 Compare(Person x, Person y) =>
+                x.LastName.CompareTo(y.LastName);
+        }
+
+        public sealed class AgeComparer : IComparer<Person>
+        {
+            public Int32 Compare(Person x, Person y) =>
+                x.Age.CompareTo(y.Age);
+        }
+
+        public sealed class BirthDateComparer : IComparer<Person>
+        {
+            public Int32 Compare(Person x, Person y) =>
+                x.BirthDate.CompareTo(y.BirthDate);
+        }
+    }
+}
--- StandartInterfaces/Program.cs
+++ StandartInterfaces/Program.cs
@@ -1,15 +1,50 @@
-﻿using System;
+﻿/**
+ * простите можно я подвину
+ * вот эту горную гряду
+ * я катаклизм я извините
+ * гряду
+ *                   © колик 
+ */
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StandartInterfaces
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            var persons = new List<Person>
+            {
+                new Person("Пушкин Александр Сергеевич", new DateTime(1799, 6, 6), new DateTime(1837, 2, 10)),
+                new Person("Высоцкий Владимир Семёнович", new DateTime(1938, 1, 25), new DateTime(1980, 7, 25)),
+                new Person("Есенин Сергей Александрович", new DateTime(1895, 10, 3), new DateTime(1925, 12, 28)),
+                new Person("Бродский Иосиф Александрович", new DateTime (1940, 5, 24), new DateTime(1996, 1, 28)),
+                new Person("Цветаева Марина Ивановна", new DateTime(1892, 10, 8), new DateTime(1941, 8, 31)),
+                new Person("Ахматова Анна Андреевна", new DateTime(1889, 6, 23), new DateTime(1966, 3, 5)),
+                new Person("Гумилёв Николай Степанович", new DateTime(1886, 4, 15), new DateTime(1921, 8, 21)),
+                new Person("Маяковский Владимир Владимирович", new DateTime(1893, 7, 19), new DateTime(1930, 4, 14))
+            };
+
+            ShowPersons(persons, "before sorting:");
+
+            persons.Sort(new Person.LastNameComparer());
+            ShowPersons(persons, "sorting by last name:");
+
+            persons.Sort(new Person.AgeComparer());
+            ShowPersons(persons, "sorting by age:");
+
+            persons.Sort(new Person.BirthDateComparer());
+            ShowPersons(persons, "sorting by birth date:");
+        }
+
+        private static void ShowPersons(IEnumerable<Person> persons, String message)
+        {
+            Console.WriteLine($"\t{message}");
+            foreach (var person in persons)
+                Console.WriteLine(person);
+            Console.WriteLine();
         }
     }
 }
