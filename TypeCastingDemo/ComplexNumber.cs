cb4ac8a2a0b204d5844ac67a77450b596648c31a
Constantine Philipchenko <const-phi@ya.ru>
2018-03-22 01:02:59 +0300

    Added demo of explicit and implicit type casting. For example ComplexNumber class and Double type were used.

---
+++ TypeCastingDemo/ComplexNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace TypeCastingDemo
+{
+    internal class ComplexNumber : IFormattable
+    {
+        private readonly Double real;
+
+        private readonly Double imaginary;
+
+        public ComplexNumber(Double re = 0, Double im = 0)
+        {
+            real = re;
+            imaginary = im;
+        }
+
+        public override String ToString() => 
+            $"<{real}; {imaginary}>";
+
+        public String ToString(String format) => 
+            ToString(format, NumberFormatInfo.CurrentInfo);
+
+        public String ToString(String format, IFormatProvider formatProvider) =>
+            $"<{real.ToString(format, formatProvider)}; {imaginary.ToString(format, formatProvider)}>";
+
+        // неявное приведение типа
+        public static implicit operator ComplexNumber(Double value) => 
+            new ComplexNumber(value);
+
+        // явное приведение типа
+        public static explicit operator Double(ComplexNumber value) =>
+            value.real;
+    }
+}
---
+++ TypeCastingDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TypeCastingDemo
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            ComplexNumber z = Math.PI;
+            Console.WriteLine($"complex value = {z:G3};");
+
+            var value = (Double) z;
+            Console.WriteLine($"real number = {value:G3};");
+        }
+    }
+}
